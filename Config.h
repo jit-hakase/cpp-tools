fc83aa3de6ecd5964ba47e2fc8e22d3f76238c64
hakase <cb634780751@qq.com>
2019-12-31 11:20:17 +0800

    Update Config.h

--- Config.h
+++ Config.h
@@ -32,3 +32,35 @@ private:
 	std::string m_file_name;
 	std::map<std::string, std::string> m_cnf_map;
 };
+
+/*
+class Config {
+public:
+	Config(std::string file_name) : m_file_name(std::move(file_name)) {}
+	
+	void Init() {
+		std::ifstream ifs(m_file_name.c_str());
+		if (!ifs.is_open()) {
+			return;
+		}
+		std::string line;
+		char key[20], value[20];
+		while (std::getline(ifs, line)) {
+			sscanf(line.c_str(), "%[^=]=%[^\n]\n", key, value);
+			m_cnf_map[key] = value;
+		}
+		ifs.close();
+	}
+	
+	std::string operator[] (std::string key) {
+		if (m_cnf_map.find(key) != m_cnf_map.end()) {
+			return m_cnf_map[key];
+		} else {
+			return "";
+		}
+	}
+private:
+	std::string m_file_name;
+	std::map<std::string, std::string> m_cnf_map;
+};
+*/
