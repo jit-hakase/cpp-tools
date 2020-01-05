c5135db168821ae89fdfa8454b74322d0f624ded
hakase <cb634780751@qq.com>
2020-01-05 16:26:23 +0800

    Update cpu_instruct.h

--- cpu_instruct.h
+++ cpu_instruct.h
@@ -3,11 +3,11 @@
 
 #include <cstdint>
 
-#define CPU_PAUSE() __asm__ __volatile__ (".byte 0xf3, 0x90")
+#define CPU_PAUSE() asm volatile (".byte 0xf3, 0x90")
 
 #define RDTSCP(val) do { \
 	uint32_t __a, __d, __c; \
-	__asm__ __volatile__ ("rdtscp" : "=a" (__a), "=d" (__d), "=c" (__c)); \
+	asm volatile ("rdtscp" : "=a" (__a), "=d" (__d), "=c" (__c)); \
 	val = (uint64_t)(__a) | ((uint64_t)(__d) << 32); \
 } while (0)
 
