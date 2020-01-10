eafc37775d962c0cee67a253c1ef9c4aef267c9f
hakase <cb634780751@qq.com>
2020-01-10 20:51:20 +0800

    Update connection.h

--- connection.h
+++ connection.h
@@ -1,96 +1,72 @@
 #ifndef CONNECTION_POOL_H
 #define CONNECTION_POOL_H
+
 #include <mutex>
 #include <queue>
 #include <memory>
 
 template <typename T>
-class Singleton {
+class SimpleConnectionPool {
 public:
-	static T* get_instance() {
-		static T obj;
-		return &obj;
+	static SimpleConnectionPool* get_instance() {
+		static SimpleConnectionPool s_conn_pool;
+		return &s_conn_pool;
 	}
-};
-
-template <typename T>
-class ConnectionPool {
-public:
-	ConnectionPool() = default;
-	~ConnectionPool() = default;
 	
 	void init(int num) {
 		std::lock_guard<std::mutex> lg(m_conn_mtx);
 		while (num--) {
-			m_conn_pool.push(std::make_shared<T>());
+			m_conn_que.push(new T);
 		}
 	}
 	
-	std::shared_ptr<T> get() {
+	T* get() {
 		std::lock_guard<std::mutex> lg(m_conn_mtx);
-		if (!m_conn_pool.empty()) {
-			auto conn = m_conn_pool.front();
-			m_conn_pool.pop();
+		if (!m_conn_que.empty()) {
+			auto conn = m_conn_que.front();
+			m_conn_que.pop();
 			return conn;
 		} else {
 			return nullptr;
 		}
 	}
 	
-	void release(std::shared_ptr<T> conn) {
+	void release(T *conn) {
 		std::lock_guard<std::mutex> lg(m_conn_mtx);
 		if (conn) {
-			m_conn_pool.push(conn);
+			m_conn_que.push(conn);
 		}
 	}
+	
 private:
-	std::queue<std::shared_ptr<T>> m_conn_pool;
+	SimpleConnectionPool() = default;
+	~SimpleConnectionPool() = default;
+	
+	std::queue<T*> m_conn_que;
 	std::mutex m_conn_mtx;
 };
 
 template <typename T>
-class Connection {
+class SimpleConnection {
 public:
-	Connection() = default;
+	SimpleConnection() = default;
+	~SimpleConnection() { release(); }
 	
-	~Connection() {
-		release();
-	}
-	
-	std::shared_ptr<T> get(int timeout = 1) {
-		while (m_conn == nullptr) {
-			m_conn = Singleton<ConnectionPool<T>>::get_instance()->get();
-			//sleep(timeout);
-		}
+	T* get() {
+		std::lock_guard<std::mutex> lg(m_conn_mtx);
+		m_conn = SimpleConnectionPool<T>::get_instance()->get();
 		return m_conn;
 	}
 	
 	void release() {
 		std::lock_guard<std::mutex> lg(m_conn_mtx);
-		Singleton<ConnectionPool<T>>::get_instance()->release(m_conn);
+		SimpleConnectionPool<T>::get_instance()->release(m_conn);
 		m_conn = nullptr;
 	}
 	
 private:
-	std::shared_ptr<T> m_conn;
+	T *m_conn = nullptr;
 	std::mutex m_conn_mtx;
 };
-
-#include <iostream>
-
-class DBA {
-public:
-	void f() {
-		std::cout << "a" << std::endl;
-	}
-};
-
-int main() {
-	Singleton<ConnectionPool<DBA>>::get_instance()->init(2);
-	std::cout << "start" << std::endl;
-	Connection<DBA> a;
-	auto x = a.get();
-	x->f();
-}
 
 #endif
